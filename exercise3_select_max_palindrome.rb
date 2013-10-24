61ef8e49b8b56d7233310ab9391857f995700290
ggoral <goral.gonzalo@gmail.com>
2013-10-24 13:09:13 -0300

    Add Exercise 3

---
+++ exercise3_select_max_palindrome.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+#Un número palíndromo se lee igual al derecho y al revés. El número palíndromo más grande obtenido de la multiplicación de dos números de 2 dígitos es 9009 (91 x 99 = 9009). Encontrá el palíndromo más grande obtenido a través de la multiplicación de dos números de 3 dígitos.
+
+def valid_palindrome 
