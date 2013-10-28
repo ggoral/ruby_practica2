5fa9d4f3b61186f424fa639ff314adc6ac06731a
ggoral <goral.gonzalo@gmail.com>
2013-10-28 15:43:12 -0300

    add exercise 4

---
+++ exercise4_select_min_sum_divisor.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+#2520 es el número más chico que puede ser dividido por cada uno de los números del 1 al 10 sin obtener resto. ¿Cual es el número más chico que puede ser dividido por cada uno de los números del 1 al 20?
