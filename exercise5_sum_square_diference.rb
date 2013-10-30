60883117f57596369dafc3252779b2bedcc1f750
ggoral <goral.gonzalo@gmail.com>
2013-10-29 22:53:12 -0300

    add exercise 5

--- exercise5_sum_square_diference.rb
+++ exercise5_sum_square_diference.rb
@@ -2,5 +2,21 @@
 # coding: utf-8
 
 #2520 es el número más chico que puede ser dividido por cada uno de los números del 1 al 10 sin obtener resto. ¿Cual es el número más chico que puede ser dividido por cada uno de los números del 1 al 20?
+#lower_bound = 10
+#1.upto(lower_bound) do |i|
+#  square += i*i
+#  sum += i
+#  end
 
-puts (1..20).inject :lcm 
+result = 0
+square = 0
+sum = 0
+1.upto(100) do |value|
+  sum += value
+  square += value**2
+end
+result = (sum**2) - square
+puts result
+
+
+
