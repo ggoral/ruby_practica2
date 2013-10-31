1cdb5c1a9fc59873c42d3bec267f3d881b7bf4ab
ggoral <goral.gonzalo@gmail.com>
2013-10-31 11:18:01 -0300

    add a simple solution

--- exercise7_sum_n_primes_under.rb
+++ exercise7_sum_n_primes_under.rb
@@ -4,11 +4,10 @@
 #La suma de los primos menores que 10 es 17 (2 + 3 + 5 + 7 = 17). Encontrá la suma de todos los primos menores que 2 millones.
 
 require 'prime'
-
+#1.upto(10).select {|x| Prime.prime?(x)}.inject(:+)
 sum = 0
 Prime.instance.each do |prime|
   break if prime >=2_000_000
   sum += prime
 end
 puts sum
-  
